ed5a267f8c27d8ce5d633f865cffa0419acfa4ab
Lisa <lisamarie181002@gmail.com>
2026-02-11 15:13:13 +0100

    a=0.4 mehr untersucht

--- Rebound_simulation/sim_with_moon.py
+++ Rebound_simulation/sim_with_moon.py
@@ -235,20 +235,20 @@ def simulation(sim):
       ecc[i, idx_moon-1] = o.e
       sma[i, idx_moon-1] = o.a / AU
     except AttributeError:
-      # idx_moon = idx_moon - 1 # when two planets collided
-      # xyz_moon[i, 0] = ps[idx_moon].x
-      # xyz_moon[i, 1] = ps[idx_moon].y
-      # xyz_moon[i, 2] = ps[idx_moon].z
-      # o = sim.particles[idx_moon].orbit(primary=sim.particles[idx_f])  # moon orbiting planet f
-      # ecc[i, 6] = o.e
-      # sma[i, 6] = o.a / AU
-      # # set the wrong data to NaN
-      # ecc[i, 5] = np.nan
-      # sma[i, 5] = np.nan
+      idx_moon = idx_moon - 1 # when two planets collided
+      xyz_moon[i, 0] = ps[idx_moon].x
+      xyz_moon[i, 1] = ps[idx_moon].y
+      xyz_moon[i, 2] = ps[idx_moon].z
+      o = sim.particles[idx_moon].orbit(primary=sim.particles[idx_f])  # moon orbiting planet f
+      ecc[i, 6] = o.e
+      sma[i, 6] = o.a / AU
+      # set the wrong data to NaN
+      ecc[i, 5] = np.nan
+      sma[i, 5] = np.nan
 
-      xyz_moon[i, :] = np.nan
-      ecc[i, 6] = np.nan
-      sma[i, 6] = np.nan
+      # xyz_moon[i, :] = np.nan
+      # ecc[i, 6] = np.nan
+      # sma[i, 6] = np.nan
 
       # if planet e gets a moon of planet f
       # a = sim.particles[4].orbit(primary=sim.particles[idx_f])  # planet e orbiting planet f
@@ -256,9 +256,9 @@ def simulation(sim):
       # sma[i, 3] = a.a / AU
 
       # if planet g gets a moon of planet f
-      a = sim.particles[6].orbit(primary=sim.particles[idx_f])  # planet g orbiting planet f
-      ecc[i, 5] = a.e
-      sma[i, 5] = a.a / AU
+      # a = sim.particles[6].orbit(primary=sim.particles[idx_f])  # planet g orbiting planet f
+      # ecc[i, 5] = a.e
+      # sma[i, 5] = a.a / AU
 
     
     
@@ -306,8 +306,8 @@ np.savetxt(f'data_with_moon_a={a_moon_short}/xyz_f_with_moon_a={a_moon_short}_m=
 np.savetxt(f'data_with_moon_a={a_moon_short}/xyz_moon_a={a_moon_short}_m={m_moon_short}.txt', xyz_moon)
 
 # Orbit plot at t=500 years
-ob1 = rebound.OrbitPlot(sim, particles=[1,2,3,4,5])
-ob2 = rebound.OrbitPlot(sim, particles=[6], primary=5, fig=ob1.fig, ax=ob1.ax, color='red')
+ob1 = rebound.OrbitPlot(sim, particles=[1,2,3,4,5,6])
+ob2 = rebound.OrbitPlot(sim, particles=[7], primary=5, fig=ob1.fig, ax=ob1.ax, color='red')
 plt.gca().set_aspect('equal', 'box')
 plt.savefig(f'plots_a={a_moon_short}/plots_m={m_moon_short}/orbit_plot_moon_500y_a={a_moon_short}_m={m_moon_short}.png', dpi=300, bbox_inches='tight')
 plt.close()
