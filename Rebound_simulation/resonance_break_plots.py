79549a2b7774701a5071356ddff185835b3495bc
lisa <lisamarie181002@gmail.com>
2026-06-10 17:44:26 +0200

    plots für rafael (mit fehler?)

---
+++ Rebound_simulation/resonance_break_plots.py
@@ -0,0 +1,134 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+m_moon_short = 0.005  # mass of the moon relative to planet f
+a_moon_short = 0.2  # semi-major axis of the moon relative to planet f
+
+##########################################################################################
+# load data
+##########################################################################################
+
+here = os.path.dirname(__file__)
+
+# filenames
+f_sma = os.path.join(here, f'data_aufgabe1_auto/data_moon_a={a_moon_short}/sma_moon_a={a_moon_short}_m={m_moon_short}.txt')
+f_ecc = os.path.join(here, f'data_aufgabe1_auto/data_moon_a={a_moon_short}/ecc_moon_a={a_moon_short}_m={m_moon_short}.txt')
+f_longitude = os.path.join(here, f'data_aufgabe1_auto/data_moon_a={a_moon_short}/l_moon_a={a_moon_short}_m={m_moon_short}.txt')
+f_omega = os.path.join(here, f'data_aufgabe1_auto/data_moon_a={a_moon_short}/omega_moon_a={a_moon_short}_m={m_moon_short}.txt')
+f_node = os.path.join(here, f'data_aufgabe1_auto/data_moon_a={a_moon_short}/orbital_node_moon_a={a_moon_short}_m={m_moon_short}.txt')
+f_inc = os.path.join(here, f'data_aufgabe1_auto/data_moon_a={a_moon_short}/inc_moon_a={a_moon_short}_m={m_moon_short}.txt')
+
+# load
+# shape: (nsteps, nplanets)
+sma = np.loadtxt(f_sma)        # semi major axis in AU
+ecc = np.loadtxt(f_ecc)       # eccentricity
+lamb = np.loadtxt(f_longitude) # mean longitude (in rad)
+omega = np.loadtxt(f_omega)    # argument of pericenter (in rad)
+Omega = np.loadtxt(f_node)     # longitude of ascending node (in rad)
+inc = np.loadtxt(f_inc)       # inclination (in deg)
+
+orbital_parameters = {
+    'Semi Major Axis (AU)':              sma,
+    'Eccentricity':                      ecc,
+    'Mean Longitude (rad)':              lamb,
+    'Argument of Pericenter (rad)':      omega,
+    'Longitude of Ascending Node (rad)': Omega,
+    'Inclination (deg)':                 inc,
+}
+
+##########################################################################################
+# Eisntellbare Parameter
+##########################################################################################
+# Was soll geplottet werden? Z.B. sma
+subject = sma
+#Welche Resonanz wird betrachtet? Psi 1, 2 oder 3
+psi_select = 2
+# Welcher Zeitraum soll geplottet werden? (in Jahren, höchstens 500)
+time_start = 300
+time_end = 400
+
+##########################################################################################
+# Hilfsfunktionen
+##########################################################################################
+def get_label(data_array, param_dict):
+    """Gibt den Namen des Arrays aus dem Dict zurück."""
+    for name, arr in param_dict.items():
+        if arr is data_array:
+            return name
+    return 'Unknown'
+
+# Convert from radians to degrees in [0,360)
+def rad_to_deg_0_360(arr):
+  a = np.mod(arr, 2*np.pi)  # now in [0, 2pi)
+  return a * 180.0 / np.pi
+
+# Calculate resonant angles
+def resonant_angles(longitude):
+  # return time series arrays (length n_steps) for each resonant angle
+
+  psi1 = longitude[:, 1] - 4 * longitude[:, 2] + 3 * longitude[:, 3]
+  psi2 = 2 * longitude[:, 2] - 5 * longitude[:, 3] + 3 * longitude[:, 4]
+  psi3 = longitude[:, 3] - 3 * longitude[:, 4] + 2 * longitude[:, 5]
+
+  psi1 = rad_to_deg_0_360(psi1)
+  psi2 = rad_to_deg_0_360(psi2)
+  psi3 = rad_to_deg_0_360(psi3)
+
+  return psi1, psi2, psi3
+
+##########################################################################################
+# Berechnung
+##########################################################################################
+psi1, psi2, psi3 = resonant_angles(lamb)
+psi_arrays = {1: psi1, 2: psi2, 3: psi3}
+psi_data   = psi_arrays[psi_select]
+
+subject_label = get_label(subject, orbital_parameters)
+
+# Zeitachse: Zeilenindex = Jahr
+n_steps = subject.shape[0]
+time    = np.linspace(0, 500, n_steps)
+mask    = (time >= time_start) & (time <= time_end)
+
+planet_labels = ['Planet b', 'Planet c', 'Planet d', 'Planet e', 'Planet f', 'Planet g']
+colors        = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown']
+
+##########################################################################################
+# Plot
+##########################################################################################
+fig, ax1 = plt.subplots(figsize=(12, 6))
+
+# --- linke Achse: subject ---
+for i, (label, color) in enumerate(zip(planet_labels, colors)):
+    ax1.plot(time[mask], subject[mask, i], label=label, color=color)
+
+ax1.set_xlabel('Time (years)')
+ax1.set_ylabel(subject_label, color='black')
+ax1.tick_params(axis='y')
+ax1.grid(alpha=0.3)
+ax1.legend(loc='upper left')
+
+# --- rechte Achse: resonant angle ---
+ax2 = ax1.twinx()
+ax2.scatter(time[mask], psi_data[mask], s=1, color='black', alpha=0.4,
+            label=rf'$\psi_{psi_select}$')
+ax2.set_ylabel(rf'$\psi_{psi_select}$ (deg)', color='black')
+ax2.set_ylim(0, 360)
+ax2.set_yticks([0, 90, 180, 270, 360])
+ax2.legend(loc='upper right')
+
+plt.title(f'{subject_label}  &  $\psi_{psi_select}$  over time\n'
+          f"(moon: $a={a_moon_short}\,R_{\text{Hill,f}}$ , $m={m_moon_short}\,m_{\text{f}}$)")
+plt.tight_layout()
+
+##########################################################################################
+# Save plot
+##########################################################################################
+output_dir = f'resonance_break_plots/a={a_moon_short}'
+os.makedirs(output_dir, exist_ok=True)
+
+plt.tight_layout()
+plt.savefig(f'{output_dir}/{subject_label}_a={a_moon_short}_m={m_moon_short}_psi{psi_select}.png', dpi=300, bbox_inches='tight')
+print(f"Saved {subject_label}_a={a_moon_short}_m={m_moon_short}_psi{psi_select}")
+plt.close()
